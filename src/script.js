5ef66852c0b4b5518e1043a7140ca7d7164b83f2
Sten Laane <laanesten@gmail.com>
2020-10-11 11:09:48 +0100

    Finally convert this project into a node package.
    Bundling with Webpack -- no more script tags everywhere.

---
+++ src/script.js
@@ -0,0 +1,77 @@
+import {RandomCaseString, github, linkedIn} from "./typedText.js";
+import Typed from "typed.js";
+import FaviconMarquee from "favicon-marquee"
+
+window.dataLayer=window.dataLayer||[];function n(){dataLayer.push(arguments)}n("js",new Date);n("config","UA-162143380-1");
+
+const faviconProps = {
+    text: 'Sten Laane 🚀',
+    font: 'Aino Headline',
+    color: '#323330',
+    size: 32,
+    step: 0.5,
+    background: "#F0DB4F",
+    marginBottom: 3,
+};
+
+const faviconMarquee = new FaviconMarquee(faviconProps);
+faviconMarquee.start(1000/24);
+
+const typedTextObserver = new MutationObserver(() => {
+    const typedText = document.querySelector(".typed");
+
+    const typedAnchorElement = typedText.querySelector("a");
+    const mirroredText = typedText.querySelector(".mirrored");
+    const typedTextCursor = typedText.parentElement.querySelector(".typed-cursor");
+
+    if (!typedText.onmousedown && typedAnchorElement) {
+        // Implementing anchor tags manually through onmousedown/onmouseup since browsers don't properly register
+        // click events on typed text because of issues with updating the tag contents https://github.com/mattboldt/typed.js/issues/214
+        // Other mouse events work properly though so this is a valid workaround
+        typedText.onmousedown = () => typedText.clickedOn = true;
+        typedText.onmouseup = () => {
+            if (typedText.clickedOn) {
+                typedText.clickedOn = false;
+                window.open(typedAnchorElement.href, "_blank");
+            }
+        }
+    } else if (typedText.onmousedown && !typedAnchorElement) {
+        typedText.onmousedown = undefined;
+        typedText.onmouseup = undefined;
+    }
+
+    if (!typedText.cursorReversed && mirroredText) {
+        typedText.before(typedTextCursor);
+        typedText.cursorReversed = true;
+    } else if (typedText.cursorReversed && !mirroredText) {
+        typedText.parentElement.appendChild(typedTextCursor);
+        typedText.cursorReversed = false;
+    }
+});
+
+const observerOptions = {
+    childList: true,
+    subtree: true,
+};
+
+typedTextObserver.observe(document.querySelector(".typed"), observerOptions);
+
+const randomCase1 = new RandomCaseString("Sten Arthur Laane^1000");
+const randomCase2 = new RandomCaseString("Sten Laane^1000");
+
+const typedTextProps = {
+    strings: [
+        'Sten Laane^1000', 'Sten Arthur Laane^1000', randomCase1,
+        'Стэн Лаане^1000', '<span class="mirrored">Sten Laane</span>^1000',
+        github,
+        linkedIn,
+        randomCase2,
+    ],
+    showCursor: true,
+    smartBackspace: true,
+    loop: true,
+    typeSpeed: 125,
+    backSpeed: 50,
+};
+
+new Typed('.typed', typedTextProps);
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const CopyPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const path = require('path');
+
+module.exports = {
+    entry: './src/script.js',
+    output: {
+        filename: 'script.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new CopyPlugin(
+        {
+            patterns: [
+                {from: "*.{html,css,png,webm,mp4,txt,svg,otf}", context: "src"}
+            ],
+        }
+    )]
+};
